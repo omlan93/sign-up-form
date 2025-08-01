2c038d0cdb9a37b13d6a2af5b724b32273d4429a
omlan93 <2105162@ugrad.cse.buet.ac.bd>
2025-08-02 01:04:54 +0600

    Password cannot contain * only

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const inputFiled = document.querySelectorAll("input")
 
 console.log(inputFiled);
 
+const regex = /^\*+$/;
+
 const password = document.querySelector("input[id='password']")
 console.log(password);
 const confirmPassword = document.querySelector("input[id='confirm_password']")
@@ -14,11 +16,15 @@ const confirmPassError = document.querySelector(".confirm-pass-span")
 
 form.addEventListener('submit', (event) => {
 
-    if (password.value != confirmPassword.value) {
+    if ((password.value != confirmPassword.value)) {
         confirmPassError.textContent = "Password and confirm password must match";
         confirmPassError.style.padding = "2px";
         event.preventDefault();
-
+    }
+    else if (regex.test(password.value)) {
+        confirmPassError.textContent = "Password cannot contain  *  Ratul Khan!";
+        confirmPassError.style.padding = "2px";
+        event.preventDefault();
     }
     else {
         confirmPassError.textContent = "";
